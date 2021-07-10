b30924214aba130f2b02b0e0a61aa89d7cfbaabb
Chuck <c.costa.gdev@gmail.com>
2021-07-10 14:29:20 +0100

    Book test part 1

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    GameObject camera;
+    public GameObject c;
+    [Range(0.01f, 0.5f)]
+    public float speed;
+
+    Vector3 target;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        target = transform.position - c.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.rotation = Quaternion.identity;
+        var r = c.transform.rotation.eulerAngles;
+        Quaternion a = Quaternion.Euler(
+            0,
+            r.y,
+            0
+        );
+        transform.rotation = Quaternion.Lerp(transform.rotation, a, speed);
+        transform.position = Vector3.Lerp(transform.position, target + c.transform.position, speed);
     }
 }
